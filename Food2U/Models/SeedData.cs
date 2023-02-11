c693650302c808f167d58a856318ffe8b5871b4a
Hogan Karr <hskarr0720@gmail.com>
2023-02-11 13:55:42 -0600

    Migrated DB

--- Food2U/Models/SeedData.cs
+++ Food2U/Models/SeedData.cs
@@ -56,29 +56,29 @@ namespace Food2U.Models
                     new Items {Name = "Chick-fil-A Deluxe", Price = Convert.ToDecimal(9.25)},
                     new Items {Name = "Spicy Southwest Salad", Price = Convert.ToDecimal(9.69)},
                     new Items {Name = "Kids Nugget Meal", Price = Convert.ToDecimal(5.85)},
-                    new Items {Name = "Vanilla Milkshake", Price = Convert.ToDecimal(4.45)}
-                //     // McDonald's Items
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    //Burger King Items
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    //Subway Items
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()},
-                //    new Items {Name = "", Price = Convert.ToDecimal()}
+                    new Items {Name = "Vanilla Milkshake", Price = Convert.ToDecimal(4.45)},
+                    // McDonald's Items
+                   new Items {Name = "Big Mac Meal", Price = Convert.ToDecimal(11.04)},
+                   new Items {Name = "Quarter Pounder with Cheese Meal", Price = Convert.ToDecimal(11.04)},
+                   new Items {Name = "10 Piece McNuggets Meal", Price = Convert.ToDecimal(10.13)},
+                   new Items {Name = "20 Piece McNuggets", Price = Convert.ToDecimal(9.09)},
+                   new Items {Name = "3 Pack Of Cookies", Price = Convert.ToDecimal(1.69)},
+                   //Burger King Items
+                   new Items {Name = "Whopper", Price = Convert.ToDecimal(6.19)},
+                   new Items {Name = "Double Whopper", Price = Convert.ToDecimal(8.69)},
+                   new Items {Name = "Triple Whopper", Price = Convert.ToDecimal(11.19)},
+                   new Items {Name = "8 Piece Chicken Nuggets", Price = Convert.ToDecimal(1.99)},
+                   new Items {Name = "Soft Serve Cup", Price = Convert.ToDecimal(1.00)},
+                   //Subway Items
+                   new Items {Name = "Meatball Marinara", Price = Convert.ToDecimal(7.29)},
+                   new Items {Name = "B.L.T", Price = Convert.ToDecimal(8.49)},
+                   new Items {Name = "Pizza Sub Melt", Price = Convert.ToDecimal(7.49)},
+                   new Items {Name = "Spicy Italian", Price = Convert.ToDecimal(6.49)},
+                   new Items {Name = "Subway Melt", Price = Convert.ToDecimal(9.49)}
                 );
 
                 db.Order.AddRange(
-
+                    // Need to add variable to calc total from items picked
                 );
             }
         }
