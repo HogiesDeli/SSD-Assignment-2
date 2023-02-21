9950e684800ea846c57c79f28d2b67780485557c
Hogan Karr <hskarr0720@gmail.com>
2023-02-21 17:49:31 -0600

    Changed how restaurants list

--- Food2U/Pages/ChooseItems.cshtml.cs
+++ Food2U/Pages/ChooseItems.cshtml.cs
@@ -1,18 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+//HK
+using Microsoft.EntityFrameworkCore;
+using Food2U.Models;
 
 namespace Food2U.Pages;
 
 public class ChooseItemsModel : PageModel
 {
+    private readonly Food2UDbContext _context;
     private readonly ILogger<ChooseItemsModel> _logger;
+    public List<Items> Items {get; set;} = default!;
 
-    public ChooseItemsModel(ILogger<ChooseItemsModel> logger)
+    public ChooseItemsModel(Food2UDbContext context, ILogger<ChooseItemsModel> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public void OnGet()
     {
+        Items = _context.Items.ToList();
     }
 }
--- Food2U/Pages/Index.cshtml.cs
+++ Food2U/Pages/Index.cshtml.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+//HK
+using Microsoft.EntityFrameworkCore;
+using Food2U.Models;
 
 namespace Food2U.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly Food2UDbContext _context;
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(Food2UDbContext context, ILogger<IndexModel> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
