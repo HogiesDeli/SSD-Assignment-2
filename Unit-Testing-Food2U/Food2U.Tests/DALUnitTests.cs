58fa6a399a538e16c0f7cd5abaa70c0a1c9fde63
DeFuanz <76855046+DeFuanz@users.noreply.github.com>
2023-03-05 20:24:09 -0600

    added unit testing from multiple pages DAL

--- Unit-Testing-Food2U/Food2U.Tests/DALUnitTests.cs
+++ Unit-Testing-Food2U/Food2U.Tests/DALUnitTests.cs
@@ -8,7 +8,9 @@ namespace Food2U.Tests;
 
 public class UnitTest1
 {
-    [Fact]
+    //Test that adding item to db works when data is correct
+    //Can be found in LocalRestaurant.cshtml.cs
+    [Fact] 
     public async Task RestaurantAddItemAsync_ItemIsAdded_WhenDataIsCorrect()
     {
         using (var db = new Food2UDbContext(Utilities.Utilities.TestDbContextOptions()))
@@ -34,18 +36,32 @@ public class UnitTest1
         }
     }
 
-    [Fact]
+    //Test that exception gets thrown if incorrect data gets entered when adding an item
+    //Can be Found in LocalRestaurant.cshtml.cs of Food2U
+    [Fact] 
     public void RestaurantAddItemAsync_ItemNotAddedShouldThrowException_WhenDataIsNotCorrect()
     {
-        
+        Assert.Throws<FormatException>(() => new Items
+        {
+            Name = "Cake",
+            Price = Convert.ToDecimal("abc"),
+            localrestaurantsID = 1
+        });
+
+    }
+
+    //Test that list still gets returned even if no Items are found
+    //Tests the method that creates lists for the dictionary found in ChooseItems.cshtml.cs
+    [Fact]
+    public async Task getRestaurantItemsAsync_DoesNotThrowException_WhenItemsAreEmpty()
+    {
         using (var db = new Food2UDbContext(Utilities.Utilities.TestDbContextOptions()))
         {
-            Assert.Throws<FormatException>(() =>  new Items
-            {
-                Name = "Cake",
-                Price = Convert.ToDecimal("abc"),
-                localrestaurantsID = 1
-            });
+            int restaurantID = 1;
+
+            List<Items> foodItems = await db.Items.Where(i => i.localrestaurantsID == restaurantID).ToListAsync();
+
+            Assert.Empty(foodItems);
         }
     }
 }
